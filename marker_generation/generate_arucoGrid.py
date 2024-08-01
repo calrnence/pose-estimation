0f43f61ba077d7a9fe15f217a0cd7a356d8d20e3
calrnence <clarencehua1@gmail.com>
2024-08-01 10:58:31 -0600

    added marker generation code
    from @ddelago

---
+++ marker_generation/generate_arucoGrid.py
@@ -0,0 +1,20 @@
+import cv2
+import cv2.aruco as aruco
+
+# Create gridboard, which is a set of Aruco markers
+# the following call gets a board of markers 5 wide X 7 tall
+gridboard = aruco.GridBoard(
+        size=(3,4), 
+        markerLength=0.1, 
+        markerSeparation=0.01, 
+        dictionary=aruco.getPredefinedDictionary(aruco.DICT_5X5_50))
+
+# Create an image from the gridboard
+img = gridboard.generateImage(outSize=(988, 1400))
+cv2.imwrite("test_gridboard.jpg", img)
+
+# Display the image to us
+cv2.imshow('Gridboard', img)
+# Exit on any key
+cv2.waitKey(0)
+cv2.destroyAllWindows()
